a7bca9b2894a8061771b5a323983e66e60b1eb9e
brylorin-prog <brylorin@gmail.com>
2026-06-27 21:57:43 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -852,7 +852,7 @@ async function showMainMenu(ctx, edit = false) {
     : '⚠️ No wallet connected';
   
   const message = `
-🚀 *Hey Welcome to Solbix Trading Bot- New* 🤖
+🚀 *Hey Welcome to Axiom auto Trading Bot- New* 🤖
 
 
 I’m the one-stop solution for all your trading needs!
